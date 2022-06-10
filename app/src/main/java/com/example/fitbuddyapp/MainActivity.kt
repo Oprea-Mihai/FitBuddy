c80adae31f6fa1ba54079574c6e97f24aaad8bd9
Madalina Serban <80857412+madalinaserban@users.noreply.github.com>
2022-06-10 14:33:55 +0300

    Bottom navigation

--- app/src/main/java/com/example/fitbuddyapp/MainActivity.kt
+++ app/src/main/java/com/example/fitbuddyapp/MainActivity.kt
@@ -1,45 +1,49 @@
 package com.example.fitbuddyapp
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import com.example.fitbuddyapp.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    public lateinit var navigationView:NavigationView
+   // public lateinit var navigationView:NavigationView
     lateinit var navController:NavController
+    private lateinit var navView: BottomNavigationView
     public val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       // val navHostFragment=supportFragmentManager.findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as NavHostFragment
-        //val navController=navHostFragment.navController
+        navView=binding.bottomView
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController=navHostFragment.navController
+       navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.signUpFragment -> hideBottomNav()
+                R.id.signInFragment->hideBottomNav()
+                R.id.splashFragment->hideBottomNav()
+           else -> showBottomNav()
+            }
 
-      //  navController.addOnDestinationChangedListener { _, destination, _ ->
-        //    when (destination.id) {
-          //      R.id.signUpFragment -> hideBottomNav()
-            //    else -> showBottomNav()
-            //}
-
-        //}
+        }
     }
     private fun showBottomNav() {
-        navigationView.visibility = View.VISIBLE
+        navView.visibility = View.VISIBLE
     }
 
     private fun hideBottomNav() {
-        navigationView.visibility = View.GONE
+        navView.visibility = View.GONE
 
     }
 }
